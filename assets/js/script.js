85b7fa14d2612daa0a4976dd78bfda6ce1e56dcd
CL <cl.czayas@gmail.com>
2022-11-01 13:11:45 -0400

    Added input spaces and save buttons but am struggling with the colorsssss

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
     $('#currentDay').text(time);
     $('#currentDay').css('font-size', '38px')
 
-    var currentHour = moment().format('hh:mm A');
-
     var hour9 = moment().hours(9).minutes(00).format('hh:mm A');
     var hour10 = moment().hours(10).minutes(00).format('hh:mm A'); 
     var hour11 = moment().hours(11).minutes(00).format('hh:mm A');
@@ -21,14 +19,27 @@ $(document).ready(function() {
     console.log(hours.length)
 
     for (var i=0; i<hours.length; i++) {
-    var row = $(`<div class="row"></div>`)
-    var col1 = $(`<div class ="col-lg-2"></div>`)
+    console.log(hours[i])
+    var row = $(`<div class="row hour"></div>`);
+    var col1 = $(`<div class ="col-lg-2"></div>`);
     $(col1).text(hours[i]);
-    var col2 = $(`<div class ="col-lg-8"</div>`)
-    var col3 = $(`<div class ="col-lg-2"></div>`)
-    row.append(col1)
-    row.append(col2)
-    row.append(col3)
-    $('.container').append(row)
-}
+    var col2 = $(`<div class ="col-lg-8 inputText"><input data-input="${i}" id="inputText${i}" class="form-control inputText" type="text" name="userInput"></div>`);
+    var col3 = $(`<div class ="col-lg-2 save-Btn"><button data-id="${i}" id="savePlanner" class="btn btn-success btn-block"><i class="fas fa-save"></i> Save</button></div>`);
+    row.append(col1);
+    row.append(col2);
+    row.append(col3);
+    $('.container').append(row);
+
+    var currentHour = moment().format('hh:mm A');
+    var hourSquare = moment(hours[i]);
+    if(hourSquare.isBefore(currentHour)) {
+        row.addClass('past');
+    }
+    else if(hourSquare.isSame(currentHour)) {
+        row.addClass('present');
+    }
+    else if(hourSquare.isAfter(currentHour)) {
+        row.addClass('future')
+    }
+    }
 })
